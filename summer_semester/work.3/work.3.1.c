621d9d59abaf51916c327320e6a3c7bc579e87a4
michael <michael.silianov@devsense.co.il>
2019-08-10 17:17:41 +0300

    First work done

--- summer_semester/work.3/work.3.1.c
+++ summer_semester/work.3/work.3.1.c
@@ -2,6 +2,47 @@
     מיכאל סיליאנוב, יבגני גולובוציוב   
 ==================================== */
 #include <stdio.h>
+#define N 3
+
+int calc(int arr1[N], int arr2[N], int *summ);
+int fillArray(int *arr, int size);
+int printArray(int arr[], int size);
+
+void main(){
+    int arr1[N], arr2[N], summ = 0;
+    
+    fillArray(arr1, N);
+    fillArray(arr2, N);
+
+    printArray(arr1, N);
+    printArray(arr2, N);
+
+    calc(arr1, arr2, &summ);
+    printf("The summ is: %d\n", summ);
+}
+
+int calc (int arr1[], int arr2[], int *summ){
+    for(int i = 0; i < N; i++){
+        *summ += (arr1[i] *arr2[i]);
+    }
+}
+
+int fillArray(int arr[], int size){
+    static int index = 1;
+    printf("Please enter %d numbers for array %d \n", size, index);
+    for(int i = 0; i < size; i++){
+        scanf("%d", &arr[i]);
+    }
+    index++;
+};
+
+int printArray(int arr[], int size){
+    printf("\n[ ");
+    for(int i = 0; i < size; i++){
+        i+1 == size ? printf("%d", arr[i]) : printf("%d, ", arr[i]) ;
+    }
+    printf(" ]\n");
+}
 
 
 
